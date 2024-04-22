f14231a353c36f9577c2694bca38d7a0a66da302
Alexander B <alexandre.bobkov@gmail.com>
2024-04-21 20:36:50 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   CMakeLists.txt
    	new file:   RP2040W_C.c

---
+++ build/CMakeFiles/picow_blink.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for picow_blink.
+# This may be replaced when dependencies are built.
---
+++ picow_blink.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+//#include "pico/stdlib.h"
+//#include "hardware/pio.h"
+//#include "hardware/clocks.h"
+#include "pico/stdlib.h"
+
+int main() {
+	const uint LED_PIN = 10;
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	stdio_init_all();
+
+	while (true) {
+		gpio_put(LED_PIN, 1);
+		sleep_ms(250);
+		gpio_put(LED_PIN, 0);
+		sleep_ms(250);
+	}
+}
