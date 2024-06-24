c86715584f7a43a45860c64f45b893ff58dafeaa
Alexander B <alexandre.bobkov@gmail.com>
2024-06-24 14:39:56 -0400

    .

---
+++ pio_blink/build/CMakeFiles/blink_blink_pio_h.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/x415ja/Programming/RP2040W_C/pio_blink/generated/blink.pio.h"
+  "CMakeFiles/blink_blink_pio_h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/blink_blink_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
